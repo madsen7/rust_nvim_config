b5252270165534ce2732f912d7aa9bd1f0529d4b
madsen <aminimohammadhasan0@gmail.com>
2025-03-27 17:17:07 +0330

    fix rust debuging

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,80 +14,81 @@ return {
   },
 
   {
-    "mrcjkb/rustaceanvim",
-    version = "^5", -- Recommended
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
     ft = "rust",
-    config = function()
-      local mason_registry = require "mason-registry"
-      local codelldb = mason_registry.get_package "codelldb"
+    config = function ()
+      local mason_registry = require('mason-registry')
+      local codelldb = mason_registry.get_package("codelldb")
       local extension_path = codelldb:get_install_path() .. "/extension/"
       local codelldb_path = extension_path .. "adapter/codelldb"
       -- local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
-      -- If you are on Linux, replace the line above with the line below:
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-      local cfg = require "rustaceanvim.config"
+	-- If you are on Linux, replace the line above with the line below:
+  	local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      local cfg = require('rustaceanvim.config')
 
       vim.g.rustaceanvim = {
         dap = {
           adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
         },
       }
-    end,
+    end
   },
 
   {
-    "rust-lang/rust.vim",
+    'rust-lang/rust.vim',
     ft = "rust",
-    init = function()
+    init = function ()
       vim.g.rustfmt_autosave = 1
-    end,
+    end
   },
 
   {
-    "mfussenegger/nvim-dap",
+    'mfussenegger/nvim-dap',
     config = function()
-      local dap = require "dap"
-      local dapui = require "dapui"
-      -- Automatically open dap-ui after the debugging session initializes
-      dap.listeners.after.event_initialized["dapui_config"] = function()
+			local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
         dapui.open()
       end
-      -- Automatically close dap-ui when debugging session terminates or exits
-      dap.listeners.before.event_terminated["dapui_config"] = function()
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
         dapui.close()
       end
-      dap.listeners.before.event_exited["dapui_config"] = function()
+      dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
       end
-    end,
+		end,
   },
 
   {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    'rcarriga/nvim-dap-ui', 
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
     config = function()
-      require("dapui").setup()
-    end,
+			require("dapui").setup()
+		end,
   },
 
   {
-    "saecki/crates.nvim",
-    ft = { "toml" },
+    'saecki/crates.nvim',
+    ft = {"toml"},
     config = function()
       require("crates").setup {
         completion = {
           cmp = {
-            enabled = true,
+            enabled = true
           },
         },
       }
-      require("cmp").setup.buffer {
-        sources = { { name = "crates" } },
-      }
-    end,
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+    end
   },
- -- {
+
+  -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
   -- 		ensure_installed = {
